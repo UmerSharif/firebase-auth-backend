0200bc6cd0a3897b6d8aeacdbea22412e5286e5d
UmerSK <umer.kdev@gmail.com>
2022-01-04 21:10:59 +0200

    Update service-account.ts

--- src/config/service-account.ts
+++ src/config/service-account.ts
@@ -1,14 +1,5 @@
  const serviceAccount = {
-  'type': 'service_account',
-  'project_id': 'fir-auth-test-4ba21',
-  'private_key_id': '1d7b4bad7a6b70814ee1ccf2c5b2effbc9470d35',
-  'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2Rshcc3Awgw7m\nngwTo2CLMiAyKWQhCNcTTOf06cFuCw5YwKohyCRDnWj0Y9rVrQ+AdC6fDOPPpiux\nEi001OIELoEVBWSWnC3nCDhCuwK2n8eXCaOea3VMOy1Kf8YoUO5FgFSd3BaXJN+b\nSdYDZ7xSlSgggUsDCDNDnPRAkX0f01sjVEVnf3H07yhnrr+g2ktn+39D8AuQtSWE\nh7o5FGiQTMb3+1clBZXGrgn+frnfSC36yfvoHJcg73J7i1+to+F3IXuujUVEXgSL\nW9Dr9TfZNDy4xw2SIXVVRLWnBbXRnLuorJrpwT4snqFJyAX/aoyk5i2heomKKMTH\n8Ks5fNkfAgMBAAECggEAHCh5dFAVppDgdj+NIGLZRmI9zLH4jg02YydldUAVNGw7\nJk/Lwys5cEPlaPecteylsT4sI5WTWHUprmbfRULrusyk/pEriwQL0p3CSeC87bxn\nfLgtprVKqguW7A7VbE36+RFMQRS1CAVXZmm0yNLiKfnGeKWwklfWdN1OyENmynRn\niZwFtEaUhaxxdAJVeIiOynOLj2Gk5KUuo2MjMUB3iZ1suNnSr+1UKyi8ArBf6ech\nC50DY688x0UYyRKqda6ZnfykTmcc8ZTcSWd93rzJt+f8k9bDPjLD/aAA6+xEKTDy\nBjrAmobF3zddXGnPO1Rj/nkBYIcNbfo1acNkoggnVQKBgQDwHa4DuTdiuv3BxY/f\nqjLbtKt3j56FsZyI4/ChqT0v2/0s7s4aK96gJ1o2riTT0LnoZCSzdetaJbDua+jk\nvzqtw8lUnjQLRXkwm8YOLhqcYTHmNFtM5tD5okBg4eovcDBxqDZELzp4mmSCpCbQ\nqwZb6iuPDQODk/7k2Ecngjrx2wKBgQDCVZo0POmtEWbjpgyum8eP/Y+OhvIofK9q\nLg8AEz1g0HjcJ/PwZpxdQv5lgcrfwgFjRsyem4CWMuOnQC9iwv5KDRyUaHBNznwH\nbr/12xJ5Q7d2U8mKaActrYNc3hgDI4HpXvueA/ohE9DwxWbKKfd8Zry6/OtKH5/9\nEyfwWcsDDQKBgEZLShBI/s/mkjdSEQaXmv/66l+8KdpV3s+se1u4YfNK4dlvp+K9\niRU1sBFl9d4Usvg3qQ7orJfdhOinqqyc1x7bvGsLTY0nL41tbRPlp2C0i5krJrdi\nszcBqvsx6A8vNt9vMUPQ17tK57XfPIXGK7xo14uvcaHph6uTLXGBjKQVAoGBAIqh\nxn+9dHWiFGQR8LofJx3xJJfobJe1Wu3ELa9ACmkmTxG+7qZwjNt7D0Aadz9ohtTr\njM0Y4YJEi2KkxykNMZpiACFe376hrpkeyJH61hLr29TtsLirvXM4dJ2rUjXq/qIc\nwCLZc3ZlTIh2Sf2uMn7sS3aZNsCKDvfa0oMDi2+BAoGASHtlc33UnZz71pZFvyCS\nMDpXYDOV3pNPWVSZiet/a/l7N6NFq8lBLvve+197Tow260/Ra1IAyZA4qyq2oK1i\n5Vc3FEBo1YwbmImd47STQVtgGS5oaLacpBUjqnVz+ckqttUTCvjbyPocJTrGqEyc\n54UIxdNAgDVC3Eu3aoaYH+U=\n-----END PRIVATE KEY-----\n',
-  'client_email': 'firebase-adminsdk-9k4v5@fir-auth-test-4ba21.iam.gserviceaccount.com',
-  'client_id': '109804547996086362249',
-  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-  'token_uri': 'https://oauth2.googleapis.com/token',
-  'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-  'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9k4v5%40fir-auth-test-4ba21.iam.gserviceaccount.com'
+  // use your own service account here
 }
 
 export default serviceAccount
